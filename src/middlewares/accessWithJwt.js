9bdf40c0997d4cace85cb90fa2fde806b80cadee
adriangonzalez-dev <adriangonzalez.dev90@gmail.com>
2022-11-22 10:24:38 -0300

    app modified

--- src/middlewares/accessWithJwt.js
+++ src/middlewares/accessWithJwt.js
@@ -2,8 +2,7 @@ const { request, response } = require("express");
 const jwt = require("jsonwebtoken");
 
 const accessWithJwt = (req = request, res = response, next) => {
-  const token = req.header("Authorization").split("Bearer ")[1];
-  console.log(token);
+  const token = req.header("Authorization")?.split("Bearer ")[1];
   if (!token) {
     return res.status(400).json({
       msg: "token is required",
